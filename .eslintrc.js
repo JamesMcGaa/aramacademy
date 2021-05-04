1930c0188e598db64a1456b68d1cf2247fa1ce1b
JamesMcGaa <jemcgaa@gmail.com>
2021-05-04 01:55:11 -0400

    React helmet for custom titles and descriptions for search prevailence

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
+    sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': 0,
+    'no-plusplus': 0,
+    camelcase: 0,
+  },
 };
