bc5b7243e5529ad3e223a25d652ca596d4916600
Tony <tony@mp1.attlocal.net>
2021-05-27 17:21:30 -0500

    ready for prod

--- src/components/user/live_game_table_desktop.js
+++ src/components/user/live_game_table_desktop.js
@@ -15,6 +15,8 @@ import Link from '@material-ui/core/Link';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
+import nunu_gif from '../../images/nunu_dancing_compressed.gif';
+import amumu_gif from '../../images/amumu_crying.gif';
 
 const globals = require('../../../globals.js');
 
@@ -27,7 +29,7 @@ const GREEN_WINRATE_CUTOFF = 52.5;
 const GREEN_KDA_CUTOFF = 4.0;
 const GREEN_COLOR = '#75ff75';
 
-const WHITE_WINRATE_CUTOFF = 50.0;
+const WHITE_WINRATE_CUTOFF = 49.9;
 const WHITE_KDA_CUTOFF = 2.5;
 const WHITE_COLOR = 'white';
 
@@ -260,209 +262,191 @@ function getUserRoute(true_summoner_name) {
   return '/users/' + params.region + '/' + encodeURI(true_summoner_name) + '/';
 }
 
-const RankBadge = (rank) => {
-  const classes = useStyles();
-  return (
-    <img
-      className={classes.rankBadge}
-      alt="ranked badge"
-      src={resources.ranked_badges[rank.rank]}
-    />
-  );
-};
-
-function TeamTable({ rows, side }) {
-  console.log('teamtable rows', rows);
-  console.log('rowslength', rows.length);
-  const classes = useStyles();
-  // const [order, setOrder] = React.useState('desc');
-  // const [orderBy, setOrderBy] = React.useState('n/a');
-  // const [selected, setSelected] = React.useState([]);
-  // const [page, setPage] = React.useState(0);
-  // const [rowsPerPage, setRowsPerPage] = React.useState(rows.length);
-  const order = 'desc';
-  const orderBy = 'n/a';
-  const selected = [];
-  const page = 0;
-  const rowsPerPage = rows.length;
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  return (
-    <Paper classes={{ root: classes.paper }}>
-      <EnhancedTableToolbar numSelected={selected.length} />
-      <TableContainer>
-        <Table
-          className={classes.table}
-          aria-labelledby="tableTitle"
-          size={'medium'} //not small
-          aria-label="enhanced table"
-        >
-          <EnhancedTableHead
-            classes={classes}
-            numSelected={selected.length}
-            order={order}
-            orderBy={orderBy}
-            onSelectAllClick={() => {}}
-            onRequestSort={() => {}}
-            rowCount={rows.length}
-            side={side}
-          />
-          <TableBody>
-            {stableSort(rows, getComparator(order, orderBy))
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => {
-                const isItemSelected = isSelected(row.summoner_name);
-                console.log('rowtablebody', row);
-                const total_winrate =
-                  row.total_games !== 0
-                    ? (row.total_wins * 100) / row.total_games
-                    : 'N/A';
-                const champ_winrate =
-                  row.champion_games !== 0
-                    ? (row.champion_wins * 100) / row.champion_games
-                    : 'N/A';
-                const champ_kda =
-                  row.champion_deaths !== 0
-                    ? (row.champion_kills + row.champion_assists) /
-                      row.champion_deaths
-                    : 'N/A';
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.summoner_name}
-                    selected={isItemSelected}
-                  >
-                    <TableCell
-                      classes={{ root: classes.iconCell }}
-                      align="center"
-                    >
-                      <img
-                        className={classes.resizeChampIcon}
-                        src={resources.champ_icons[row.champion]}
-                      />
-                    </TableCell>
-
-                    <TableCell align="left">
-                      <Link
-                        href={getUserRoute(row.summoner_name)}
-                        style={{ textDecoration: 'none' }}
-                        color="inherit"
-                      >
-                        {row.summoner_name}
-                      </Link>
-                    </TableCell>
-
-                    <TableCell align="right">
-                      {RankBadge({ rank: row.rank })}
-                      {row.mmr}
-                    </TableCell>
-
-                    <TableCell align="right">
-                      {' '}
-                      {typeof total_winrate === 'number' ? (
-                        <span
-                          style={{
-                            color: winrateColor(total_winrate),
-                          }}
-                        >
-                          {total_winrate.toFixed(2)}%
-                        </span>
-                      ) : (
-                        'N/A'
-                      )}
-                    </TableCell>
-
-                    <TableCell align="right">
-                      {typeof champ_winrate === 'number' ? (
-                        <span
-                          style={{
-                            color: winrateColor(champ_winrate),
-                          }}
-                        >
-                          {champ_winrate.toFixed(2)}%
-                        </span>
-                      ) : (
-                        'N/A'
-                      )}
-                    </TableCell>
-                    <TableCell align="right">{row.champion_games}</TableCell>
-                    <TableCell align="right">
-                      {typeof champ_kda === 'number' ? (
-                        <span
-                          style={{
-                            color: kdaColor(champ_kda),
-                          }}
-                        >
-                          {champ_kda.toFixed(2)}:1
-                        </span>
-                      ) : (
-                        'N/A'
-                      )}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
-  );
-}
 const LiveGameLoading = ({ summoner_name }) => {
   const classes = useStyles();
-
   return (
     <div>
-      <Paper classes={{ root: classes.paper }}>
-        <Typography variant="h2">
-          Searching {summoner_name} in Live Game API
-        </Typography>
-      </Paper>
+      <h3>
+        <img src={nunu_gif} style={{ width: '500px' }} alt="loading..." />
+      </h3>
+      <Typography variant="h5">
+        Searching {summoner_name} in Live Game API
+        <br /> This may take a while due to our slow Riot API key
+      </Typography>
     </div>
   );
 };
 
 const LiveGameNoMatch = ({ summoner_name }) => {
   const classes = useStyles();
-
   return (
     <div>
-      <Paper classes={{ root: classes.paper }}>
-        <Typography variant="h2">
-          {summoner_name} is not in a live game.
-        </Typography>
-      </Paper>
+      <h3>
+        <img src={amumu_gif} style={{ width: '500px' }} alt="loading..." />
+      </h3>
+      <Typography variant="h5">
+        {summoner_name} is not in a live ARAM
+      </Typography>
     </div>
   );
 };
 
 const LiveGameMatch = ({ full_live_game_data }) => {
-  // const a1 = {
-  //   champion: 'Viego',
-  //   summoner_name: 'DeusExAnimo',
-  //   mmr: 2500,
-  //   champion_games: 200,
-  //   total_wins: 130,
-  //   total_games: 300,
-  //   champion_wins: 102,
-  //   champion_kills: 13.48,
-  //   champion_deaths: 8.3,
-  //   rank: 'challenger',
-  //   champion_assists: 5.2,
-  // };
-  // return TeamTable({ rows: [a1], side: TEAM_SIDE.BLUE });
+  const classes = useStyles();
+
+  function TeamTable({ rows, side }) {
+    const RankBadge = (rank) => {
+      return (
+        <img
+          className={classes.rankBadge}
+          alt="ranked badge"
+          src={resources.ranked_badges[rank.rank]}
+        />
+      );
+    };
+    console.log('teamtable rows', rows);
+    console.log('rowslength', rows.length);
+
+    const order = 'desc';
+    const orderBy = 'n/a';
+    const selected = [];
+    const page = 0;
+    const rowsPerPage = rows.length;
+
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+
+    return (
+      <Paper classes={{ root: classes.paper }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={'medium'} //not small
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={() => {}}
+              onRequestSort={() => {}}
+              rowCount={rows.length}
+              side={side}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.summoner_name);
+                  console.log('rowtablebody', row);
+                  const total_winrate =
+                    row.total_games !== 0
+                      ? (row.total_wins * 100) / row.total_games
+                      : 'N/A';
+                  const champ_winrate =
+                    row.champion_games !== 0
+                      ? (row.champion_wins * 100) / row.champion_games
+                      : 'N/A';
+                  const champ_kda =
+                    row.champion_deaths !== 0
+                      ? (row.champion_kills + row.champion_assists) /
+                        row.champion_deaths
+                      : 'N/A';
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.summoner_name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell
+                        classes={{ root: classes.iconCell }}
+                        align="center"
+                      >
+                        <img
+                          className={classes.resizeChampIcon}
+                          src={resources.champ_icons[row.champion]}
+                        />
+                      </TableCell>
+
+                      <TableCell align="left">
+                        <Link
+                          href={getUserRoute(row.summoner_name)}
+                          style={{ textDecoration: 'none' }}
+                          color="inherit"
+                        >
+                          {row.summoner_name}
+                        </Link>
+                      </TableCell>
+
+                      <TableCell align="right">
+                        {RankBadge({ rank: row.rank })}
+                        {row.mmr}
+                      </TableCell>
+
+                      <TableCell align="right">
+                        {' '}
+                        {typeof total_winrate === 'number' ? (
+                          <span
+                            style={{
+                              color: winrateColor(total_winrate),
+                            }}
+                          >
+                            {total_winrate.toFixed(2)}%
+                          </span>
+                        ) : (
+                          'N/A'
+                        )}
+                      </TableCell>
+
+                      <TableCell align="right">
+                        {typeof champ_winrate === 'number' ? (
+                          <span
+                            style={{
+                              color: winrateColor(champ_winrate),
+                            }}
+                          >
+                            {champ_winrate.toFixed(2)}%
+                          </span>
+                        ) : (
+                          'N/A'
+                        )}
+                      </TableCell>
+                      <TableCell align="right">{row.champion_games}</TableCell>
+                      <TableCell align="right">
+                        {typeof champ_kda === 'number' ? (
+                          <span
+                            style={{
+                              color: kdaColor(champ_kda),
+                            }}
+                          >
+                            {champ_kda.toFixed(2)}:1
+                          </span>
+                        ) : (
+                          'N/A'
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    );
+  }
 
   let blueTeam = [];
   let redTeam = [];
@@ -489,188 +473,35 @@ export default function LiveGameTable({ summoner_name, live_game_status }) {
   function handleDataFetch(live_game_data) {
     if (fullLiveGameData === null) {
       setFullLiveGameData(live_game_data);
+      console.log('full', fullLiveGameData);
     }
-    console.log('full', fullLiveGameData);
   }
   const params = useParams();
   const [fullLiveGameData, setFullLiveGameData] = useState(null);
 
   useEffect(() => {
-    fetch(
-      '/api/live_game/' +
-        encodeURI(params.region) +
-        '/' +
-        encodeURI(params.summonerName)
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        handleDataFetch(json.full_data);
-      });
+    if (fullLiveGameData === null) {
+      fetch(
+        '/api/live_game/' +
+          encodeURI(params.region) +
+          '/' +
+          encodeURI(params.summonerName)
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          handleDataFetch(json.full_data);
+        });
+    }
   });
-  console.log('livegamestatus', live_game_status);
 
   if (live_game_status === globals.LIVE_GAME_STATES.NO_MATCH) {
     return LiveGameNoMatch({ summoner_name });
-  }
-  if (
+  } else if (
     live_game_status === globals.LIVE_GAME_STATES.MATCH &&
     fullLiveGameData === null
   ) {
     return LiveGameLoading({ summoner_name });
+  } else {
+    return LiveGameMatch({ full_live_game_data: fullLiveGameData });
   }
-
-  return LiveGameMatch({ full_live_game_data: fullLiveGameData });
-
-  // const [state, setState] = useState({
-  //     page_state: live_game_status,
-  //     full_data: null,
-  // });
-
-  // const [fullData, setFullData] = useState(null);
-  // function handleLiveGameResponse(json) {
-  //     console.log('handlelivegame', json);
-  //     setFullData(json.full_data);
-
-  // }
-  // let return_component;
-  // if(live_game_status === globals.LIVE_GAME_STATES.NO_MATCH){
-  //     return_component = <LiveGameNoMatch summoner_name={summoner_name} />;
-
-  // }
-  // else if(live_game_status === globals.LIVE_GAME_STATES.MATCH && fullData === null){
-  //     fetch('/api/live_game/' + params.region + '/' + encodeURI(summoner_name))
-  //         .then((response) => response.json())
-  //         .then((json) => {
-  //             handleLiveGameResponse(json);
-  //         });
-  //     console.log('loading');
-  //     return_component= <LiveGameLoading summoner_name={summoner_name} />;
-  // }
-
-  // if(state.full_data === null){
-
-  //     if (state.page_state === globals.LIVE_GAME_STATES.MATCH ){
-  //         fetch('/api/live_game/' + params.region + '/' + encodeURI(summoner_name))
-  //             .then((response) => response.json())
-  //             .then((json) => {
-  //                 handleLiveGameResponse(json);
-  //             });
-  //         console.log('loading');
-  //         return <LiveGameLoading summoner_name={summoner_name} />;
-  //     }
-  //     if (state.page_state === globals.LIVE_GAME_STATES.NO_MATCH) {
-  //         return <LiveGameNoMatch summoner_name={summoner_name} />;
-  //     }
-
-  // }
-
-  // let blueTeam = [];
-  // let redTeam = [];
-  // let blueSide;
-  // let redSide;
-  // console.log('fulldata', fullData);
-  // if(fullData !== null){
-  //     for (let i = 0; i < fullData.length; i++) {
-  //         const row = fullData[i];
-  //         if (row.side === TEAM_SIDE.BLUE) {
-  //             blueTeam.push(row);
-  //         }
-  //         else {
-  //             redTeam.push(row);
-  //         }
-  //     }
-
-  // }
-  // console.log('blueteam',blueTeam);
-  // console.log('redteam', blueTeam);
-
-  // blueSide = TeamTable({ rows: blueTeam, side: TEAM_SIDE.BLUE });
-  // redSide = TeamTable({ rows: redTeam, side: TEAM_SIDE.RED });
-
-  const a1 = {
-    champion: 'Viego',
-    summoner_name: 'DeusExAnimo',
-    mmr: 2500,
-    champion_games: 200,
-    total_wins: 130,
-    total_games: 300,
-    champion_wins: 102,
-    champion_kills: 13.48,
-    champion_deaths: 8.3,
-    rank: 'challenger',
-    champion_assists: 5.2,
-  };
-  return TeamTable({ rows: [a1], side: TEAM_SIDE.BLUE });
-
-  //   if(fullData !== null){
-  //       return_component = <div> {blueSide}{redSide}</div>
-
-  //   }
-  //     return (
-  //         return_component
-  //     );
-  //   const a1 = {
-  //     champion: 'Viego',
-  //     summoner_name: 'DeusExAnimo',
-  //     mmr: 2500,
-  //     champion_games: 200,
-  //     winrate: 50.38,
-  //     champion_winrate: 47.1,
-  //     kills: 13.48,
-  //     deaths: 8.3,
-  //     rank: 'challenger',
-  //     assists: 5.2,
-  //   };
-  //   const a2 = {
-  //     champion: 'Khazix',
-  //     summoner_name: 'Kirito Sensei',
-  //     mmr: 2500,
-  //     champion_games: 300,
-  //     winrate: 34.38,
-  //     champion_winrate: 56.1,
-  //     kills: 3.48,
-  //     deaths: 3.3,
-  //     rank: 'bronze',
-  //     assists: 24.2,
-  //   };
-  //   const a3 = {
-  //     champion: 'Lulu',
-  //     summoner_name: 'expectverylittle',
-  //     mmr: 2500,
-  //     champion_games: 400,
-  //     winrate: 49.38,
-  //     champion_winrate: 50.1,
-  //     kills: 3.48,
-  //     deaths: 38.3,
-  //     rank: 'diamond',
-  //     assists: 5.2,
-  //   };
-  //   const a4 = {
-  //     champion: 'Nautilus',
-  //     summoner_name: 'Gated',
-  //     mmr: 2500,
-  //     champion_games: 20,
-  //     winrate: 51.38,
-  //     champion_winrate: 39.1,
-  //     kills: 13.48,
-  //     deaths: 3.3,
-  //     rank: 'master',
-  //     assists: 5.2,
-  //   };
-  //   const a5 = {
-  //     champion: 'Kaisa',
-  //     summoner_name: 'ianviv',
-  //     mmr: 2500,
-  //     champion_games: 10,
-  //     winrate: 53.38,
-  //     champion_winrate: 47.1,
-  //     kills: 3.48,
-  //     deaths: 3.3,
-  //     rank: 'silver',
-  //     assists: 47.2,
-  //   };
-
-  //   const rows = [a1, a2, a3, a4, a5];
-  //   const blueSide = TeamTable({ rows: rows, side: TEAM_SIDE.BLUE });
-  //   const redSide = TeamTable({ rows: rows, side: TEAM_SIDE.RED });
 }
